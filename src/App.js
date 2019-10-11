d79601b9c35c1c3cac7ddd3d2c97c0f4de9bac7a
rkudik001c <RamaKrishna_Kudikala@cable.comcast.com>
2019-10-11 18:05:31 -0400

    Added stop click handler to fetch timings

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Stop from "./Stop";
 import './App.css';
 import axios from 'axios';
+import Timings from "./Timings";
 
 class App extends React.Component {
 
@@ -9,10 +10,11 @@ class App extends React.Component {
   {
     super();
      this.state =   {
-       "data":{
+       
        "stops" : [
-          ]
-       }
+          ],
+        "timings":[]
+       
      };
      this.handleStopClick = this.handleStopClick.bind(this);
   }
@@ -20,22 +22,46 @@ class App extends React.Component {
   componentDidMount() {
     axios.get("https://transit.land/api/v1/stops?served_by=o-dr4e-portauthoritytransitcorporation")
       .then(res => {
-        this.setState(res);
+      
+        this.setState((prevState)=>{
+          
+          return {"stops":res.data.stops,"timings":[]};
+
+        });
       });
   }
 
   handleStopClick(id){
     console.log("clicked", id);
+    const timingsGetUrl = "https://transit.land/api/v1/schedule_stop_pairs?feed_onestop_id=f-dr4e-patco&origin_onestop_id="+id+"&origin_departure_between=08:00:00,08:20:00&date=today";
+    axios.get(timingsGetUrl)
+         .then(
+           res => {
+            this.setState((prevState)=>{
+          
+              return {
+                      "stops":prevState.stops,
+                      "timings":res.data.schedule_stop_pairs.map(t=>t.origin_departure_time)
+                    };
+    
+            });
+           }
+         );
   }
   
  render(){
-    const stopItems = this.state.data.stops.map(
+    const stopItems = this.state.stops.map(
       item=> <Stop key={item.onestop_id} stopItem={item} handleStopClick={this.handleStopClick} />
     );
   
   return (
-    <div className="stopsList">
-      {stopItems}
+    <div>
+      <div className="stopsList">
+        {stopItems}
+      </div>
+      <div>
+          <Timings departureTimes={this.state.timings} />
+      </div>
     </div>
   );
  }
--- src/Timings.js
+++ src/Timings.js
@@ -3,18 +3,11 @@ import React from 'react';
 class Timings extends React.Component{
     
 
-    componentDidMount(){
-
-        //https://transit.land/api/v1/schedule_stop_pairs?
-        //feed_onestop_id=f-dr4e-patco&origin_onestop_id=s-dr4dv05cxp-ashland&
-        //origin_departure_between=08:00:00,08:20:00&date=today
-    }
-    
     render(){
-    
-        return <div>
-            Timings block
-        </div>
+         console.log(this.props);
+        return <ul>
+            {this.props.departureTimes.map(i=><li>{i}</li>)}
+        </ul>
     }
 }
 
